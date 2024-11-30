5368427f209960d005b1c438eef569a1bb4dd9ca
MarcosHenriqueBarbosaPereira <marcos.h@gec.inatel.br>
2024-11-30 18:42:46 -0300

    refactor: remove unused UI components and update application structure
    Co-authored-by: Pedro A. <pedr.augustobarbosa.aparecido@gmail.com>

--- c214_lab_project/application/ui/application.py
+++ c214_lab_project/application/ui/application.py
@@ -1,34 +1,12 @@
-from tkinter import Tk, ttk
-
-import sv_ttk
-
-from c214_lab_project.application.ui.screens import Home, SignIn
+import webview
 
 
 class Application:
     def __init__(self) -> None:
-        self._root = Tk(screenName="Master Upload Storage")
-
-        self._root.title("Master Upload Storage")
-        self._root.geometry("1280x720")
-
-        self._frames = {
-            "home": Home(parent=self),
-            "sign_in": SignIn(parent=self),
-        }
-
-        for frame in self._frames.values():
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.navigate_to("sign_in")
-        self._config_theme()
+        pass
 
     def run(self):
-        self._root.mainloop()
-
-    def _config_theme(self):
-        sv_ttk.set_theme("dark")
-
-    def navigate_to(self, screen_name: str):
-        screen = self._frames[screen_name]
-        screen.tkraise()
+        webview.settings["ALLOW_DOWNLOADS"] = True
+        webview.settings["ALLOW_FILE_URLS"] = True
+        webview.create_window("Upload File master", "./templates/index.html")
+        webview.start()
--- c214_lab_project/application/ui/screens/__init__.py
+++
@@ -1,3 +0,0 @@
-from .home import *
-from .sign_in import *
-from .sign_up import *
